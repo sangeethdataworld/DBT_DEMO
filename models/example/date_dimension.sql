c18c2ef9c3a29c9176d7236bdf9eef0e8d4e29f9
sangeeth878787@gmail.com <sangeeth878787@gmail.com>
2026-06-02 05:34:37 +0000

    dimensions

--- models/example/date_dimension.sql
+++ models/example/date_dimension.sql
@@ -1,6 +1,5 @@
-with cte as 
-(
-SELECT
+WITH CTE AS (
+select
 TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
 DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
 HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
@@ -17,10 +16,10 @@ CASE WHEN MONTH(TO_TIMESTAMP(STARTED_AT)) in (12,1,2)
     THEN 'SUMMER'
     ELSE 'AUTUMN' 
     END AS STATION_OF_YEAR
-
- from
-    {{ source('demo', 'bike') }}
-    WHERE STARTED_AT != 'started_at'
+from
+{{ source('demo', 'bike') }}
+where STARTED_AT != 'started_at' 
 )
-
-SELECT * FROM cte
+select 
+*
+from CTE
